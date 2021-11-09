6b1c2c1927a5c033a73f01976c0644405a96a228
sendoh_glb <847378761_glb@qq.com>
2021-11-09 10:14:02 +0100

    conflict test master

--- src/main/java/tum/git/gittest.java
+++ src/main/java/tum/git/gittest.java
@@ -6,5 +6,6 @@ public class gittest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git3!");
+        System.out.println("conflict test master");
     }
 }
