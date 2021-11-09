d9791f0209d0eba2f8d2e54ce607f7a73ea6487f
sendoh_glb <847378761_glb@qq.com>
2021-11-09 09:59:33 +0100

    second commit

--- src/main/java/tum/git/gittest.java
+++ src/main/java/tum/git/gittest.java
@@ -3,6 +3,7 @@ package tum.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
 
     }
 }
